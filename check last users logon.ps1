1813a2a6712812a0a21cd1007da9cff04cca6220
yonioliel <olielyoni@gmail.com>
2019-06-25 09:43:20 +0300

    Add files via upload

---
+++ check last users logon.ps1
@@ -0,0 +1,3 @@
+﻿$Path = 'C:\LastLogon.csv'
+Get-ADUser -Filter * -Properties * | 
+Select Name,@{l='OU';e={$_.DistinguishedName.split(',')[1].split('=')[1]}},@{Name="TIME"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp).ToString('dd-MM-yy')}} | Export-Csv -Path $Path –notypeinformation
